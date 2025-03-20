b64dbd40e9d7f675299c665b68770e76326b0ef7
MarciaMoreno <mapat.soad@gmail.com>
2025-03-19 21:28:12 -0300

    Finalização da pagina hmtl e css do desafio

---
+++ Desafio de Projeto - Classificador de Nivel de Herói/index.js
@@ -0,0 +1,8 @@
+
+function abrirModal() {
+    document.getElementById("meuModal").style.display = "block";
+}
+
+function fecharModal() {
+    document.getElementById("meuModal").style.display = "none";
+}
