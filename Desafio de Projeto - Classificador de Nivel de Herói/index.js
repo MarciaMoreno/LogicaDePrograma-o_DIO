a95ba61a77477ad0409c67cbce9ebdaf149cfdf6
MarciaMoreno <mapat.soad@gmail.com>
2025-03-20 10:50:34 -0300

    Ajustes de estilo e adição dos niveis de herois

--- Desafio de Projeto - Classificador de Nivel de Herói/index.js
+++ Desafio de Projeto - Classificador de Nivel de Herói/index.js
@@ -1,8 +1,19 @@
+    /**Janela Modal  */
+    function abrirModal() {
+        document.getElementById("meuModal").style.display = "block";
+    }
 
-function abrirModal() {
-    document.getElementById("meuModal").style.display = "block";
-}
+    function fecharModal() {
+        document.getElementById("meuModal").style.display = "none";
+    }
 
-function fecharModal() {
-    document.getElementById("meuModal").style.display = "none";
-}
+/* Sorteio dos números de moedas */
+    function sortearNumero(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function realizarSorteio() {
+        const numeroSorteado = sortearNumero(1000, 10000);
+        document.getElementById("resultado").innerText = numeroSorteado + " Moedas";
+    }
+
