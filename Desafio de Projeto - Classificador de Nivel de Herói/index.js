5b20c51809424d4f0b27652fadd2fa928ca4aa08
MarciaMoreno <mapat.soad@gmail.com>
2025-03-20 14:33:26 -0300

    Update de html

--- Desafio de Projeto - Classificador de Nivel de Herói/index.js
+++ Desafio de Projeto - Classificador de Nivel de Herói/index.js
@@ -12,24 +12,24 @@
 
 /**** Trabalhando os modais  */
 
-    // Obter elementos da página
-    const modal = document.getElementById("myModal");
-    const openModalBtn = document.getElementById("openModalBtn");
-    const closeBtn = document.getElementById("closeBtn");
+     // Pegando os elementos
+     var modal = document.getElementById("myModal");
+     var btn = document.getElementById("openModal");
+     var span = document.getElementsByClassName("close")[0];
 
-    // Abrir a modal quando o botão for clicado
-    openModalBtn.onclick = function() {
-        modal.style.display = "block";
-    }
+     // Abrir o modal
+     btn.onclick = function() {
+         modal.style.display = "block";
+     }
 
-    // Fechar a modal quando o botão de fechar for clicado
-    closeBtn.onclick = function() {
-        modal.style.display = "none";
-    }
+     // Fechar o modal ao clicar no "X"
+     span.onclick = function() {
+         modal.style.display = "none";
+     }
 
-    // Fechar a modal se o usuário clicar fora da janela modal
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+     // Fechar ao clicar fora do modal
+     window.onclick = function(event) {
+         if (event.target == modal) {
+             modal.style.display = "none";
+         }
+     }
