1420fd55d1dc913bcccca813c29423c40979553f
MarciaMoreno <mapat.soad@gmail.com>
2025-03-21 22:43:17 -0300

    Finalização do desafio

--- Desafio de Projeto - Classificador de Nivel de Herói/index.js
+++ Desafio de Projeto - Classificador de Nivel de Herói/index.js
@@ -1,19 +1,136 @@
-/************************************************************************************************ */
     /* Sorteio dos números de moedas */
+
+        let numeroSorteado = 0; // Variável global
+
         function sortearNumero(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min
         }
 
         function realizarSorteio() {
-            const numeroSorteado = sortearNumero(1000, 10000)
-            document.getElementById("resultado").innerText = numeroSorteado + " Moedas... Agora escolha o seu herói!... "
-        }     
+            numeroSorteado = sortearNumero(1000, 10100)
+            document.getElementById("resultado").innerText = numeroSorteado + " Moedas... Agora escolha o seu herói!... ";
+        } 
+        
+        function calcular(){
+            let nivel01 = 1000;
+            let nivel02 = 2000;
+            if( numeroSorteado < nivel01){
+                document.getElementById("ponto").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel01 && numeroSorteado < nivel02){
+                document.getElementById("ponto").innerText = "FERRO!... Você possui " + numeroSorteado +" Moedas"
+                document.getElementById("mensagem").innerText = "Busque mais recompensas na pedra do poder, e suba de nivel!..."
+            }else{
+                document.getElementById("ponto").innerText = "FERRO!... Mas você possui " + numeroSorteado +" Moedas..."
+                document.getElementById("mensagem").innerText = "QUE TAL SUBIR DE NIVEL.. O bronze é o ideal para o seu XP!..."
+            }
+        }
+        
+        function calcular2(){
+            let nivel02 = 2000;
+            let nivel03 = 5000;
+            if( numeroSorteado < nivel02){
+                document.getElementById("ponto2").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem2").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel02 && numeroSorteado < nivel03){
+                document.getElementById("ponto2").innerText = "BRONZE!... Você possui " + numeroSorteado +" moedas"
+                document.getElementById("mensagem2").innerText = "Busque mais recompensas na pedra do poder, e suba de nivel!..."
+            }else{
+                document.getElementById("ponto2").innerText = "BRONZE!... Mas você possui " + numeroSorteado +" moedas..."
+                document.getElementById("mensagem2").innerText = "QUE TAL SUBIR DE NIVEL.. O PRATA é o ideal para o seu XP!..."
+            }
+        }
+
+        function calcular3(){
+            let nivel03 = 5000;
+            let nivel04 = 7000;
+            if( numeroSorteado < nivel03){
+                document.getElementById("ponto3").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem3").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel03 && numeroSorteado < nivel04){
+                document.getElementById("ponto3").innerText = "PRATA!... Você possui " + numeroSorteado +" moedas"
+                document.getElementById("mensagem3").innerText = "Busque mais recompensas na pedra do poder, e suba de nivel!..."
+            }else{
+                document.getElementById("ponto3").innerText = "PRATA!... Mas você possui " + numeroSorteado +" moedas..."
+                document.getElementById("mensagem3").innerText = "QUE TAL SUBIR DE NIVEL.. O OURO é o ideal para o seu XP!..."
+            }
+        }
+
+        function calcular4(){
+            let nivel04 = 7000;
+            let nivel05 = 8000;
+            if( numeroSorteado < nivel04){
+                document.getElementById("ponto4").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem4").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel04 && numeroSorteado < nivel05){
+                document.getElementById("ponto4").innerText = "OURO!... Você possui " + numeroSorteado +" moedas"
+                document.getElementById("mensagem4").innerText = "Busque mais recompensas na pedra do poder, e suba de nivel!..."
+            }else{
+                document.getElementById("ponto4").innerText = "OURO!... Mas você possui " + numeroSorteado +" moedas..."
+                document.getElementById("mensagem4").innerText = "QUE TAL SUBIR DE NIVEL.. O PLATINA é o ideal para o seu XP!..."
+            }
+        }
+
+        function calcular5(){
+            let nivel05 = 8000;
+            let nivel06 = 9000;
+            if( numeroSorteado < nivel05){
+                document.getElementById("ponto5").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem5").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel05 && numeroSorteado < nivel06){
+                document.getElementById("ponto5").innerText = "PLATINA!... Você possui " + numeroSorteado +" moedas"
+                document.getElementById("mensagem5").innerText = "Busque mais recompensas na pedra do poder, e suba de nivel!..."
+            }else{
+                document.getElementById("ponto5").innerText = "PLATINA!... Mas você possui " + numeroSorteado +" moedas..."
+                document.getElementById("mensagem5").innerText = "QUE TAL SUBIR DE NIVEL.. O ASCENDENTE é o ideal para o seu XP!..."
+            }
+        }
+
+        function calcular6(){
+            let nivel06 = 9000;
+            let nivel07 = 10000;
+            if( numeroSorteado < nivel06){
+                document.getElementById("ponto6").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem6").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel06 && numeroSorteado < nivel07){
+                document.getElementById("ponto6").innerText = "ASCENDENTE!... Você possui " + numeroSorteado +" moedas"
+                document.getElementById("mensagem6").innerText = "Busque mais recompensas na pedra do poder, e suba de nivel!..."
+            }else{
+                document.getElementById("ponto6").innerText = "ASCENDENTE!... Mas você possui " + numeroSorteado +" moedas..."
+                document.getElementById("mensagem6").innerText = "QUE TAL SUBIR DE NIVEL.. O IMORTAL é o ideal para o seu XP!..."
+            }
+        }
+
+        function calcular7(){
+            let nivel07 = 10000;
+            let nivel08 = 10100;
+            if( numeroSorteado < nivel07){
+                document.getElementById("ponto7").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem7").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel07 && numeroSorteado < nivel08){
+                document.getElementById("ponto7").innerText = "IMORTAL!... Você possui " + numeroSorteado +" moedas"
+                document.getElementById("mensagem7").innerText = "Busque mais recompensas na pedra do poder, e suba de nivel!..."
+            }else{
+                document.getElementById("ponto7").innerText = "IMORTAL!... Mas você possui " + numeroSorteado +" moedas..."
+                document.getElementById("mensagem7").innerText = "QUE TAL SUBIR DE NIVEL.. O RADIANTE é o ideal para o seu XP!..."
+            }
+        }
+
+        function calcular8(){
+            let nivel08 = 10100;
+            if( numeroSorteado < nivel08){
+                document.getElementById("ponto8").innerText = "OPS! Não foi dessa vez!... Você possui apenas " + numeroSorteado +" moedas"
+                document.getElementById("mensagem8").innerText = "Busque mais recompensas na pedra do poder!..."
+            }else if( numeroSorteado >= nivel08 ){
+                document.getElementById("ponto8").innerText = "RADIANTE!... Você possui " + numeroSorteado +" moedas"
+                document.getElementById("mensagem8").innerText = "OPS! Estamos no ultimo nível... Agora você é IMORTAL!..."
+            }
+        }
            
 
     /*********************************************************************************************** */
     /**** Trabalhando os modais  */
       
-
     function abrirModal(){
         var modal = document.getElementById('janela-modal');
         modal.classList.add('abrir')
@@ -100,10 +217,4 @@
 
     /*********************************************************************************************** */
     /**** Trabalhando os NIVEIS DE PONTOS DOS HERÓIS...  */
-
-    function calcular() {
-            
-            
-            // Exibir o resultado no HTML
-           
-        }
+    
